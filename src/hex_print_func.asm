968a9db82305960687d4fa1e930c75d89fcb2e04
Lawrence Warren <lawrencewarren2@gmail.com>
2021-08-25 23:24:11 +0100

    Got my hex printer working! In a non-generic case

--- src/hex_print_func.asm
+++ src/hex_print_func.asm
@@ -7,23 +7,23 @@ hex_print_func:
   add   bx, 0d2       ; move past '0x', leave us pointing at first position
 
   ; first char
-  mov   ax, 0x0000
   mov   ah, dh
-  and   ah, 0xf0
-  shr   ah, 0d4
-  add   ah, 0d48
+  shr   ax, 0d4
+  ;cmp   ah, 0d10
+  ;jl    hex_print_is_int
+  ;jge   hex_print_is_char
+  add   ah, 0x30
   mov   [bx], ah
   add   bx, 0d1
-
+  
   ; second char
-  ; mov   ax, 0x0000
-  ; mov   ah, dh
-  ; and   ah, 0x0f
-  ; shr   ah, 0d4
-  ; add   ah, 0d65
-  ; mov   [bx], ah
-  ; add   bx, 0d1
-
+  mov   ah, al
+  shr   ax, 0d4
+  ;jl    hex_print_is_int
+  ;jge   hex_print_is_char
+  add   ah, 0x37
+  mov   [bx], ah
+  add   bx, 0d1
 
   ; done!
   mov   bx, HEX_OUT
@@ -32,29 +32,11 @@ hex_print_func:
   ret
 
 hex_print_is_int:
+  add   ah, 0x30
   ret
 
 hex_print_is_char:
+  add   ah, 0x37
   ret
 
 
-
-; This is C code for doing what we want
-
-; std::string view_chararr2hexstrbe(int count,
-;                                   unsigned char* values,
-;                                   bool prepend = false) {
-;   char ret[count * 2 + 1];
-;   char* ptr = ret;
-; 
-;   while (count--) {
-;     g_snprintf(ptr, 3, "%02X", (int)values[count]);
-;     ptr += 2;  // Step string pointer
-;   }
-; 
-;   if (prepend) {
-;     return std::string(ret).insert(0, "0x");
-;   } else {
-;     return std::string(ret);
-;   }
-; }
