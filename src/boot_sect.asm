599c2c9e6da1dc152a70b293e4cfd8ab8f5648c4
Lawrence Warren <lawrencewarren2@gmail.com>
2021-08-26 18:02:09 +0100

    Updated boot_sect.asm to reflect future use

--- src/boot_sect.asm
+++ src/boot_sect.asm
@@ -9,19 +9,34 @@
   mov bp, 0x8000          ; Here we set our stack safely out of the
   mov sp, bp              ; way , at 0 x8000
 
-  mov   bx, HELLO_MSG
-  call  string_print_func
 
-  mov   bx, GOODBYE_MSG
-  call  string_print_func
 
-  mov   dx, 0x1fb6
-  call  hex_print_func
+
+
+
+
+
+
+  ; Reading some extra sectors into memory
+
+  mov bx, 0x9000          ; Load 5 sectors to 0x0000 (ES):0x9000 (BX)
+  mov dh, 2               ; from the boot disk.
+
+  mov dl, [BOOT_DRIVE]
+  call disk_load
+  
+  mov dx, [0x9000]        ; Print out the first loaded word, which
+  call hex_print_func     ; we expect to be 0xdada, stored
+                          ; at address 0x9000
+  
+  mov dx, [0x9000 + 512]  ; Also , print the first word from the
+  call hex_print_func     ; 2nd loaded sector : should be 0 xface
 
   jmp   $ ; Hang
 
 %include "src/string_print_func.asm"
 %include "src/hex_print_func.asm"
+%include "src/disk_load.asm"
 
 ; Data
 HELLO_MSG:
@@ -31,7 +46,7 @@ GOODBYE_MSG:
   db  'Goodbye, world!', 0
 
 HEX_OUT:
-  db '0x0000',0
+  db '0x0000', 0
 
 ; Global variables
 BOOT_DRIVE: 
@@ -47,3 +62,6 @@ BOOT_DRIVE:
 
   dw  0xaa55 ; Last two bytes ( one word ) form the magic number ,
             ; so BIOS knows we are a boot sector.
+
+times 256 dw 0xdada
+times 256 dw 0xface
