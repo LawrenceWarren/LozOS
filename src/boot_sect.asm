e08d3933b51c6c6c755a9d58fd151c69f74cf859
Lawrence Warren <lawrencewarren2@gmail.com>
2021-08-28 14:20:56 +0100

    Update boot-sector and add relevant functions [BROKEN]
    prints repeated 'S' characters - need to look closer at code

--- src/boot_sect.asm
+++ src/boot_sect.asm
@@ -3,41 +3,41 @@
 
 [org 0x7c00] ; Tells the assembler where this code will be loaded
 
-  mov [BOOT_DRIVE], dl    ; BIOS stores our boot drive in DL , so it ’s
-                          ; best to remember this for later.
+  mov bp, 0x9000          ; Here we set our stack safely out of the
+  mov sp, bp              ; way , at 0x8000
 
-  mov bp, 0x8000          ; Here we set our stack safely out of the
-  mov sp, bp              ; way , at 0 x8000
+  mov bx, MSG_REAL_MODE
+  call string_print_func
 
+  call switch_to_pm ; Note that we never return from here.
 
+  jmp $
 
-
-
-
-
-
-
-  ; Reading some extra sectors into memory
-
-  mov bx, 0x9000          ; Load 5 sectors to 0x0000 (ES):0x9000 (BX)
-  mov dh, 2               ; from the boot disk.
-
-  mov dl, [BOOT_DRIVE]
-  call disk_load
-  
-  mov dx, [0x9000]        ; Print out the first loaded word, which
-  call hex_print_func     ; we expect to be 0xdada, stored
-                          ; at address 0x9000
-  
-  mov dx, [0x9000 + 512]  ; Also , print the first word from the
-  call hex_print_func     ; 2nd loaded sector : should be 0 xface
-  
-  jmp   $ ; Hang
-
+%include "src/string_print_func_32.asm"
 %include "src/string_print_func.asm"
-%include "src/hex_print_func.asm"
-%include "src/disk_load.asm"
 %include "src/gdt.asm"
+%include "src/switch_to_pm.asm"
+
+[bits 32]
+
+; This is where we arrive after switching to and initialising protected mode.
+BEGIN_PM:
+  mov ebx, MSG_PROT_MODE
+  call string_print_func_32 ; Use our 32 - bit print routine.
+
+  jmp $ ; Hang
+
+  ; Global variables
+MSG_REAL_MODE:
+  db 'Started in 16-bit Real Mode', 0
+MSG_PROT_MODE:
+  db 'Successfully landed in 32-bit Protected Mode', 0
+  
+
+
+  ; Bootsector padding
+  times 510-($-$$) db 0
+  dw 0xaa55
 
 
 
@@ -49,27 +49,3 @@
 
 
 
-
-
-
-; Data
-HEX_OUT:
-  db '0x0000', 0
-
-; Global variables
-BOOT_DRIVE: 
-  db 0
-
-; PUT NOTHING BELOW HERE!
-
-  times 510-($-$$) db 0 ; When compiled , our program must fit into 512 bytes ,
-                        ; with the last two bytes being the magic number ,
-                        ; so here , tell our assembly compiler to pad out our
-                        ; program with enough zero bytes (db 0) to bring us to the
-                        ; 510 th byte.
-
-  dw  0xaa55 ; Last two bytes ( one word ) form the magic number ,
-            ; so BIOS knows we are a boot sector.
-
-times 256 dw 0xdada
-times 256 dw 0xface
--- src/string_print_func.asm
+++ src/string_print_func.asm
@@ -3,14 +3,14 @@
 ; Function enters here, assuming bx is setup as a parameter
 string_print_func:
   pusha
-  mov   ah, 0x0e ; int =10/ ah =0 x0e -> BIOS tele - type output
+  mov   ah, 0x0e ; int =10/ ah =0x0e -> BIOS tele-type output
   mov   al, [bx]
   jmp   string_print_main_loop
 
 string_print_main_loop:
   cmp   al, 0
   je    string_print_end
-  int   0x10 ; print the character in al
+  int   0x10  ; print the character in al
   add   bx, 1 ; pointer arithmetic - increment pointer by one
   mov   al, [bx]
   jmp   string_print_main_loop
